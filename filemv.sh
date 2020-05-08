01ef1ad1c7bf7be4a73eb59b14651c9e7f4d24fe
blotsome <32025097+blotsome@users.noreply.github.com>
2020-05-08 08:52:05 -0400

    add comment text

--- filemv.sh
+++ filemv.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 #function to convert to title case, used in Directory location
+#by stackoverflow user agc
 ftc() { set ${*,,} ; set ${*^} ; echo -n "$1 " ; shift 1 ; \
         for f in ${*} ; do \
             case $f in  A|The|Is|Of|And|Or|But|About|To|In|By) \
@@ -9,13 +10,18 @@ ftc() { set ${*,,} ; set ${*^} ; echo -n "$1 " ; shift 1 ; \
             esac ; \
         done ; echo ; }
 
+#set 3 regex expressions to isolate show title, season # and episode #
+#season/episode should be formated s#e#, but case or leading zero shouldn't matter
 regex='(.+)[\.][sS][0-9|1-9]+'
 regex2='[sS]([0-9|1-9]+)'
 regex3='[eE]([0-9|1-9]+)'
 
-for f in *.mkv           # no need to use ls.
+#for all .mkv files in active directory
+for f in *.mkv
 
 do
+    #attempt to locate be searching before the season, then converting periods to spaces
+    #and standardizing captialization. extra work needed for non standard RuPaul capitalization
     if [[ $f =~ $regex ]]; then
         filename=${BASH_REMATCH[1]//./ }
         filename=$(ftc $filename)
@@ -27,6 +33,8 @@ do
     else
         echo "No match found!"
     fi
+    
+    #attempt to identify season #
     if [[ $f =~ $regex2 ]]; then
         season=${BASH_REMATCH[1]}
         echo "FOUND SEASON: ${season}"
@@ -34,7 +42,7 @@ do
         echo "No season found!"
     fi
 
-
+    #attempt to identify episode #
     if [[ $f =~ $regex3 ]]; then
         episode=${BASH_REMATCH[1]}
         echo "FOUND EPISODE: ${episode}"
@@ -42,8 +50,10 @@ do
         echo "No episode found!"
     fi
 
-
+    #sets directory target based on information found above
     dir="../../TV/$filename/Season $season/"
+    #and if that directory exists, move file there, and if not, and new season (episode 1)
+    #identified, then create that directory first before moving
     if [[ -d $dir ]]; then 
         echo "Directory $dir exists!"
         mv -vn "$f" "$dir"
@@ -54,7 +64,4 @@ do
     else
         echo "Directory not found for $dir, did not move."
     fi
-
 done
-
-
