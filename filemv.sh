427c452ebd6b4bb255d89a2119a60de7d22bd9cd
blotsome <32025097+blotsome@users.noreply.github.com>
2020-05-08 18:16:30 -0400

    Major update: adding quite and test mode flag options

--- filemv.sh
+++ filemv.sh
@@ -10,6 +10,25 @@ ftc() { set ${*,,} ; set ${*^} ; echo -n "$1 " ; shift 1 ; \
             esac ; \
         done ; echo ; }
 
+#establish variables for different modes
+quiet_flag=''
+test_flag=''
+
+#create usage function
+print_usage() {
+  printf '%b\n' "Usage: -q for quiet mode and -t to test the command without making any changes to the file system"
+}
+
+#getopts to get option flags
+while getopts 'qtvh' flag; do
+  case "${flag}" in
+    q) quiet_flag='true' ;;
+    t) test_flag='true' ;;
+    h | *) print_usage
+       exit 1 ;;
+  esac
+done
+
 #set 3 regex expressions to isolate show title, season # and episode #
 #season/episode should be formated s#e#, but case or leading zero shouldn't matter
 regex='(.+)[\.][sS][0-9|1-9]+'
@@ -29,40 +48,70 @@ do
             filename=$(echo "$filename" | sed 's/rupaul/RuPaul/gi')
         fi
         filename="$(echo -e "$filename" | sed -e 's/[[:space:]]*$//')"
-        echo "FOUND MATCH: ${filename}"
+        if ! [[ $quiet_flag ]]; then
+          echo "FOUND MATCH: ${filename}"
+        fi
     else
-        echo "No match found!"
+        if ! [[ $quiet_flag ]]; then
+          echo "No match found!"
+        fi
     fi
-    
+
     #attempt to identify season #
     if [[ $f =~ $regex2 ]]; then
         season=${BASH_REMATCH[1]}
         season=$(printf %02d $season)
-        echo "FOUND SEASON: ${season}"
+        if ! [[ $quiet_flag ]]; then
+          echo "FOUND SEASON: ${season}"
+        fi
     else
-        echo "No season found!"
+        if ! [[ $quiet_flag ]]; then
+          echo "No season found!"
+        fi
     fi
 
     #attempt to identify episode #
     if [[ $f =~ $regex3 ]]; then
         episode=${BASH_REMATCH[1]}
-        echo "FOUND EPISODE: ${episode}"
+        if ! [[ $quiet_flag ]]; then
+          echo "FOUND EPISODE: ${episode}"
+        fi
     else
-        echo "No episode found!"
+        if ! [[ $quiet_flag ]]; then
+          echo "No episode found!"
+        fi
     fi
 
     #sets directory target based on information found above
     dir="../../TV/$filename/Season $season/"
     #and if that directory exists, move file there, and if not, and new season (episode 1)
     #identified, then create that directory first before moving
-    if [[ -d $dir ]]; then 
-        echo "Directory $dir exists!"
-        mv -vn "$f" "$dir"
+    if [[ -d $dir ]]; then
+        if ! [[ $quiet_flag ]]; then
+          echo "Directory $dir exists!"
+        fi
+        if ! [[ $test_flag ]]; then
+          if ! [[ $quiet_flag ]]; then
+            mv -vn "$f" "$dir"
+          else
+            mv -n "$f" "$dir"
+          fi
+        fi
     elif [[ $episode = "01" || $episode = "1" ]]; then
-        echo "New season detected!"
-        mkdir "$dir"
-        mv -vn "$f" "$dir"
+        if ! [[ $quiet_flag ]]; then
+          echo "New season detected!"
+        fi
+        if ! [[ $test_flag ]]; then
+          mkdir "$dir"
+          if ! [[ $quiet_flag ]]; then
+            mv -vn "$f" "$dir"
+          else
+            mv -n "$f" "$dir"
+          fi
+        fi
     else
-        echo "Directory not found for $dir, did not move."
+        if ! [[ $quiet_flag ]]; then
+          echo "Directory not found for $dir, did not move."
+        fi
     fi
 done
